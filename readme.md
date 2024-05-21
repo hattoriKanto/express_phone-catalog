eda6dba7cb8e0a568c610f9009da55bd8f0db9b6
Nirvacsh <nir.vlad.zu@gmail.com>
2024-05-21 13:25:34 +0300

    Swagger added and readme

---
+++ readme.md
@@ -0,0 +1,15 @@
+### now based on develop branch - need to change it later
+
+[DEPLOYED PROJECT ON RENDER.COM](https://express-phone-catalog.onrender.com)
+
+## Used technologies
+
+- Prisma
+- PostgreSQL
+- Swagger
+- Express.js
+- Eslint
+- Prettier
+- Husky
+- Typescript
+- Lint-staged
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Hello World API',
+      version: '1.0.0',
+      description: 'A simple Express Hello World API',
+    },
+  },
+  // Path to the API docs
+  apis: ['./**/*.ts'], // Adjust this later
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/', (_, res) => {
   res.send('Hello World!');
