d89c80023c540cbadadbae533dda4f2759c79844
Olena Karnaukh <lenakarnaukh648@gmail.com>
2024-05-22 23:10:25 +0300

    Add products get endpoint with pagination

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -6,9 +6,16 @@ type GetAll = (req: Request, res: Response) => void;
 
 export const getAll: GetAll = async (req, res) => {
   try {
+    const { size, page } = req.query;
     const { category } = req.params;
-    const products = await productsServices.getAll(category);
-
+    const params: { size?: number; page?: number } = {};
+    if (typeof Number(size) === 'number') {
+      params.size = Number(size);
+    }
+    if (typeof Number(page) === 'number') {
+      params.page = Number(page);
+    }
+    const products = await productsServices.getAll(category, params);
     res.status(HTTPCodes.OK).send(products);
   } catch (error) {
     if (error instanceof Error) {
