da932996bfe3feea86a42224087e4f67248cc95d
Nirvacsh <nir.vlad.zu@gmail.com>
2024-05-29 01:22:35 +0300

    hotfix

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(cookieParser());
 app.use(express.static('public'));
 
